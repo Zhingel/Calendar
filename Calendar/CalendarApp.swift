14f7bb3934c4b3a04967c301012f6fc1de3965d3
Stas <staszhingel@icloud.com>
2021-01-21 10:56:07 +0300

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Стас Жингель on 21.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
